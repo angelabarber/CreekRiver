83488977884c1bcb848b60785c6b5a0d7b8e43cc
Angela Barber <147751213+angelabarber@users.noreply.github.com>
2024-09-27 16:32:26 -0500

    adding DTO

---
+++ Models/DTOs/ReservationDTO.cs
@@ -0,0 +1,11 @@
+namespace CreekRiver.Models
+
+{
+    public class ReservationDTO
+    {
+        public int Id { get; set; }
+        public DateTime ReservationDate { get; set; }
+
+        public string Status { get; set; }
+    }
+}
