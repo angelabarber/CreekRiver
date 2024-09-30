e2274708fe55539884884a6ec47448cca1a3a929
Angela Barber <147751213+angelabarber@users.noreply.github.com>
2024-09-30 16:42:33 -0500

    migrations!

---
+++ Migrations/20240930214022_InitialCreate.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CreekRiver.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CampsiteTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CampsiteTypeName = table.Column<string>(type: "text", nullable: false),
+                    MaxReservationDays = table.Column<int>(type: "integer", nullable: false),
+                    FeePerNight = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CampsiteTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfiles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Campsites",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nickname = table.Column<string>(type: "text", nullable: false),
+                    ImageUrl = table.Column<string>(type: "text", nullable: false),
+                    CampsiteTypeId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Campsites", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Campsites_CampsiteTypes_CampsiteTypeId",
+                        column: x => x.CampsiteTypeId,
+                        principalTable: "CampsiteTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CampsiteId = table.Column<int>(type: "integer", nullable: false),
+                    UserProfileId = table.Column<int>(type: "integer", nullable: false),
+                    CheckinDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CheckoutDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Campsites_CampsiteId",
+                        column: x => x.CampsiteId,
+                        principalTable: "Campsites",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservations_UserProfiles_UserProfileId",
+                        column: x => x.UserProfileId,
+                        principalTable: "UserProfiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CampsiteTypes",
+                columns: new[] { "Id", "CampsiteTypeName", "FeePerNight", "MaxReservationDays" },
+                values: new object[,]
+                {
+                    { 1, "Tent", 15.99m, 7 },
+                    { 2, "RV", 26.50m, 14 },
+                    { 3, "Primitive", 10.00m, 3 },
+                    { 4, "Hammock", 12m, 7 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "UserProfiles",
+                columns: new[] { "Id", "Email", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "johndoe@example.com", "John", "Doe" },
+                    { 2, "janesmith@example.com", "Jane", "Smith" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Campsites",
+                columns: new[] { "Id", "CampsiteTypeId", "ImageUrl", "Nickname" },
+                values: new object[,]
+                {
+                    { 1, 1, "https://tnstateparks.com/assets/images/content-images/campgrounds/249/colsp-area2-site73.jpg", "Barred Owl" },
+                    { 2, 2, "https://tnstateparks.com/assets/images/content-images/campgrounds/249/colsp-area2-site73.jpg", "River Birch" },
+                    { 3, 3, "https://tnstateparks.com/assets/images/content-images/campgrounds/249/colsp-area2-site73.jpg", "Whitetail Ridge" },
+                    { 4, 4, "https://tnstateparks.com/assets/images/content-images/campgrounds/249/colsp-area2-site73.jpg", "Fox Den" },
+                    { 5, 1, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhx-wyYEIrvUJlQq2oQqJYTs_cwGeyLniYaA&s", "Cedar Hollow" },
+                    { 6, 2, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-TAXidJ1UKsB_Kpaav_98bvHAFNrUkfd4dQ&s", "Pine Haven" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Reservations",
+                columns: new[] { "Id", "CampsiteId", "CheckinDate", "CheckoutDate", "UserProfileId" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2024, 6, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 6, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), 1 },
+                    { 2, 2, new DateTime(2024, 7, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 7, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), 2 },
+                    { 3, 3, new DateTime(2024, 8, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 8, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), 1 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Campsites_CampsiteTypeId",
+                table: "Campsites",
+                column: "CampsiteTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_CampsiteId",
+                table: "Reservations",
+                column: "CampsiteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_UserProfileId",
+                table: "Reservations",
+                column: "UserProfileId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservations");
+
+            migrationBuilder.DropTable(
+                name: "Campsites");
+
+            migrationBuilder.DropTable(
+                name: "UserProfiles");
+
+            migrationBuilder.DropTable(
+                name: "CampsiteTypes");
+        }
+    }
+}
